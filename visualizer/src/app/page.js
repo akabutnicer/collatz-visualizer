0ac9570ede31b4c5bafae7e7cf9b530782ff30d0
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2025-02-08 21:21:45 -0500

    Update page.js

--- visualizer/src/app/page.js
+++ visualizer/src/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import Form from "./components/Form";
 export default function Home() {
+  const submit = (_) => {
+        _.preventDefault();
+        const { integer } = Object.fromEntries(new FormData(_.target));
+  };
   return (
     <>
-       <Form />
+       <Form onSubmit={submit} />
     </>
   )
 }
